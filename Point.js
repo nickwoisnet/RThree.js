6e4b8929f3a219ae7d4bc6eb4c173fd152b245cd
nickwoisnet <nickwoisnet@gmail.com>
2015-05-04 19:24:54 -0700

    Included color property for a point.

--- Point.js
+++ Point.js
@@ -1,9 +1,10 @@
 // Nicholas Woisnet
 // 5/4/2015
 // Simple Point
-function Point (X,Y,Z) {
+function Point (X,Y,Z,Color) {
 	var self = this;
 	self.X = X;
 	self.Y = Y;
 	self.Z = Z;
+	self.Color = Color;
 }
--- PointSample.js
+++ PointSample.js
@@ -1,6 +1,6 @@
 var Points = [];
-Points.push(new Point(200,200,0));
-Points.push(new Point(300,200,0));
-Points.push(new Point(100,200,0));
-Points.push(new Point(100,100,0));
-Points.push(new Point(200,300,0));
+Points.push(new Point(200,200,0, '#D2291U'));
+Points.push(new Point(300,200,0, '#00FFFF'));
+Points.push(new Point(100,200,0, '#006200'));
+Points.push(new Point(100,100,0, '#7B2CE2'));
+Points.push(new Point(200,300,0, '#4B1083'));
