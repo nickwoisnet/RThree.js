dd602a99811319a7fda3c84dabcc1f91881805e6
nickwoisnet <nickwoisnet@gmail.com>
2015-05-04 19:19:17 -0700

    It is hard to tell on my screen but I think I see a couple blue points. Probably add color property for each point next.

---
+++ Point.js
@@ -0,0 +1,9 @@
+// Nicholas Woisnet
+// 5/4/2015
+// Simple Point
+function Point (X,Y,Z) {
+	var self = this;
+	self.X = X;
+	self.Y = Y;
+	self.Z = Z;
+}
---
+++ PointSample.js
@@ -0,0 +1,6 @@
+var Points = [];
+Points.push(new Point(200,200,0));
+Points.push(new Point(300,200,0));
+Points.push(new Point(100,200,0));
+Points.push(new Point(100,100,0));
+Points.push(new Point(200,300,0));
